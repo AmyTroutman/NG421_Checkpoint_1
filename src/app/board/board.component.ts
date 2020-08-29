5de36a2b23ff5daa44060d358352aeb25bc5289d
AmyTroutman <amyrtroutman@gmail.com>
2020-08-28 21:47:54 -0500

    Can drag and drop, but gets stuck in done. also need to match status to column

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ITodo } from '../interfaces/itodo';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-board',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+ todos: ITodo[];
+ doing: ITodo[] = [];
+ done: ITodo[] = [];
 
-  constructor() { }
-
+  constructor(private todoService: TodoService, private cdRef: ChangeDetectorRef) { }
+//   ngAfterViewChecked() {
+//     this.cdRef.detectChanges();
+//  }
   ngOnInit() {
+    this.todos = this.todoService.getTodos();
   }
-
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      event.item.data(t => t.status = event.container);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
